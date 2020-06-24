c32f2c3d6aa16ac24150816deb83c721217932ce
Henrique Rodrigues <henryqrm@gmail.com>
2020-06-24 01:46:10 -0300

    feat: add suport to NX

--- bin/runner.js
+++ bin/runner.js
@@ -1,6 +1,6 @@
 module.exports = (flags) => {
 	// Imports.
-	const getWorkspacesYarn = require("../lib/getWorkspacesYarn");
+	const getWorkspacesNx = require("../lib/getWorkspacesNx");
 	const multiSemanticRelease = require("../lib/multiSemanticRelease");
 	const multisemrelPkgJson = require("../package.json");
 	const semrelPkgJson = require("semantic-release/package.json");
@@ -14,7 +14,7 @@ module.exports = (flags) => {
 		console.log(`semantic-release version: ${semrelPkgJson.version}`);
 
 		// Get list of package.json paths according to Yarn workspaces.
-		const paths = getWorkspacesYarn(cwd);
+		const paths = getWorkspacesNx(cwd);
 		console.log("yarn paths", paths);
 
 		// Do multirelease (log out any errors).
